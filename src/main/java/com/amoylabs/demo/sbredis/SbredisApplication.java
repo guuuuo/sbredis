00723deebca3b191fb0c742211ac3571e6aa756d
Niko Guo <niko.guo@ehealth.com>
2020-12-09 14:31:49 +0800

    Initial commit

---
+++ src/main/java/com/amoylabs/demo/sbredis/SbredisApplication.java
@@ -0,0 +1,13 @@
+package com.amoylabs.demo.sbredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbredisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amoylabs/demo/sbredis/SbredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoylabs.demo.sbredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
